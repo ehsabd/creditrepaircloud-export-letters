f3c9cf721957e74cd4527f91996137e2b6518a0f
ehsabd <ehsabd@outlook.com>
2021-02-27 21:50:23 +0330

    CUSTOM_EXT_NAME env variable to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@
 build() {
     echo "EXPORT_BUTTON_TEXT is ${EXPORT_BUTTON_TEXT}"
     echo "SUCCESS_MESSAGE is ${SUCCESS_MESSAGE}"
+    echo "CUSTOM_EXT_NAME is ${CUSTOM_EXT_NAME}"
     echo 'building react'
 
     rm -rf dist/*
@@ -21,7 +22,7 @@ build() {
 
     sed "s|<EXPORT_BUTTON_TEXT>|${EXPORT_BUTTON_TEXT:-'Export Letters'}|" -i dist/crc-export-letters.js
     sed "s|<SUCCESS_MESSAGE>|${SUCCESS_MESSAGE:-'Letters Have Been Exported Successfully'}|" -i dist/crc-export-letters.js
-    
+    sed "s|CreditRepairCloud Export Letters|${CUSTOM_EXT_NAME:-'CreditRepairCloud Export Letters'}|" -i dist/manifest.json
     }
 
 build
