b21a1c4dda25711bfe88ccb33034f02211c6898e
ehsabd <ehsabd@outlook.com>
2021-02-28 11:41:10 +0330

    add other address fields to test spec

--- spec/crcletterparser/spec.js
+++ spec/crcletterparser/spec.js
@@ -4,8 +4,7 @@ describe("Letter Parser", function () {
     
     it("Should parse the letter correctly with the standard format", function () {
 
-        const item = {
-            content: `<div class="pageBreak" style="page-break-after:always; display: inline-block;">
+            content= `<div class="pageBreak" style="page-break-after:always; display: inline-block;">
             <p>Sample Client <br />
              1234 Main Street<br />Santa Monica, California 90401<br />
              Date of Birth: 02/14/1963<br />SS#: 1111</p>
@@ -13,13 +12,26 @@ describe("Letter Parser", function () {
             <p>01/25/2021</p>
             <p>Re: Letter to Remove Inaccurate Credit Information</p>
             <p>To Whom It May Concern:</p>
-            <p>I received a copy of my credit report and found the following item(s) to be in error:</p>`,
-            from: { address_state: 'California' },
-            to: { address_state: 'PA' }
-        }; 
-        const output = parseLetter(item.content);
-        expect(output.from.address_state).toEqual(item.from.address_state);
-        expect(output.to.address_state).toEqual(item.to.address_state);
+            <p>I received a copy of my credit report and found the following item(s) to be in error:</p>`
+        
+        const output = parseLetter(content);
+        expect(output).toEqual({
+            from: {
+              address_country: 'US',
+              name: 'Sample Client',
+              address_city: 'Santa Monica',
+              address_state: 'California',
+              address_zip: '90401'
+            },
+            to: {
+              address_country: 'US',
+              name: 'TransUnion LLC Consumer Dispute Center',
+              address_city: 'Chester',
+              address_state: 'PA',
+              address_zip: '45678'
+            },
+            ssn: ' 1111'
+          });
     
 
 
@@ -28,8 +40,7 @@ describe("Letter Parser", function () {
 
     it("Should parse the letter correctly when the destination address has an extra line", function () {
 
-        const item =  {
-            content: `<p>Sonya Beavers</p>
+            content = `<p>Sample Client</p>
             <p>1111 McKinnon Avenue APT 3333</p>
             <p> San Francisco, California 23456</p>
             <p> Date of Birth: ---</p>
@@ -39,19 +50,31 @@ describe("Letter Parser", function () {
             <p> Chester, PA 12345</p>
             <p> Attn: Fraud Department</p>
             <p> 01/01/2021</p>
-            <p> <span> To Whom It May Concern,</span></p>`,
-            from: { address_state: 'California' },
-            to: { address_state: 'PA' }
-        };
-        const output = parseLetter(item.content);
-        expect(output.from.address_state).toEqual(item.from.address_state);
-        expect(output.to.address_state).toEqual(item.to.address_state);
+            <p> <span> To Whom It May Concern,</span></p>`;
+       
+        const output = parseLetter(content);
+        expect(output).toEqual({
+            from: {
+              address_country: 'US',
+              name: 'Sample Client',
+              address_city: 'San Francisco',
+              address_state: 'California',
+              address_zip: '23456'
+            },
+            to: {
+              address_country: 'US',
+              name: 'Dest Name2',
+              address_city: 'Chester',
+              address_state: 'PA',
+              address_zip: '12345'
+            },
+            ssn: ' 1113'
+          });
     });
 
     it("Should parse the letter correctly when the SS# comes before Date of Birth", function () {
 
-        const item =  {
-            content: `<p>Sonya Beavers</p>
+            content = `<p>Sample Client</p>
             <p>1111 McKinnon Avenue APT 3333</p>
             <p> San Francisco, California 23456</p>
             <p> SS#: 1113</p>
@@ -61,12 +84,26 @@ describe("Letter Parser", function () {
             <p> Chester, PA 12345</p>
             <p> Attn: Fraud Department</p>
             <p> 01/01/2021</p>
-            <p> <span> To Whom It May Concern,</span></p>`,
-            from: { address_state: 'California' },
-            to: { address_state: 'PA' }
-        };
-        const output = parseLetter(item.content);
-        expect(output.from.address_state).toEqual(item.from.address_state);
+            <p> <span> To Whom It May Concern,</span></p>`;
+
+        const output = parseLetter(content);
+        expect(output).toEqual({
+            from: {
+              address_country: 'US',
+              name: 'Sample Client',
+              address_city: 'San Francisco',
+              address_state: 'California',
+              address_zip: '23456'
+            },
+            to: {
+              address_country: 'US',
+              name: 'Dest Name2',
+              address_city: 'Chester',
+              address_state: 'PA',
+              address_zip: '12345'
+            },
+            ssn: ' 1113'
+          });
         expect(output.to.address_state).toEqual(item.to.address_state);
     });
 
