62320bbdc6bec143175134bd57350795b4035b42
ehsabd <ehsabd@outlook.com>
2021-03-22 14:47:44 +0430

    initial lob formatting: add ssn and dob before letter date

--- public/crc-letter-formatter.js
+++ public/crc-letter-formatter.js
@@ -1,4 +1,19 @@
+const findIndexOfLetterDate = (content) => {
+    const dateregex = /(<br \/>|<p>)\s*[0-9]+\/[0-9]+\/[0-9]{4}/;
+    return content.match(dateregex).index;
+}
+
 const formatLetter = (content, format) => {
+    if (typeof parseLetter === 'undefined') {
+        var parseLetter = require('./crc-letter-parser');
+    }
+    if (format === 'lob'){
+        const parsedLetter = parseLetter(content);
+        const letterDateIndex = findIndexOfLetterDate(content);
+        return content.substring(0, letterDateIndex) + 
+        `<p>Date of Birth: ${parsedLetter.dob}<br />SS#: ${parsedLetter.ssn}</p>` +
+        content.substring(letterDateIndex); 
+    }
     return content;
 }
 
