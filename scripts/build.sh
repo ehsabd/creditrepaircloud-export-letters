cf8b180ed65816e127f46424d13c3882efa4b1dc
ehsabd <ehsabd@outlook.com>
2021-03-31 17:23:11 +0430

    add zip archive of ext when building

--- scripts/build.sh
+++ scripts/build.sh
@@ -23,6 +23,10 @@ build() {
     sed "s|<EXPORT_BUTTON_TEXT>|${EXPORT_BUTTON_TEXT:-'Export Letters'}|" -i dist/crc-export-letters.js
     sed "s|<SUCCESS_MESSAGE>|${SUCCESS_MESSAGE:-'Letters Have Been Exported Successfully'}|" -i dist/crc-export-letters.js
     sed "s|CreditRepairCloud Export Letters|${CUSTOM_EXT_NAME:-'CreditRepairCloud Export Letters'}|" -i dist/manifest.json
+    
+    ver=`jq -r .version dist/manifest.json`
+    zip -r ./archives/dist-v$ver.zip ./dist
+
     }
 
 build
